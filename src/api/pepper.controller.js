464fd36fec4733bcf85eaade53638b6febf62456
Matt <lumpychum@gmail.com>
2023-03-09 14:59:27 -0500

    Update pepper.controller.js

--- src/api/pepper.controller.js
+++ src/api/pepper.controller.js
@@ -5,16 +5,9 @@ let fnum = 0;
 
 export default class PepperCtrl {
     static async getPepper(req, res, next) {
-        let key = req.query.key
+        console.log(req.headers);
 
-        if (key == process.env.KEY) {
-            fnum += 1;
-            console.log(`shared ${fnum} times`);
-            res.json({ pepper: process.env.PEPPER });
-
-        } else {
-            res.status(404).json({error: "400 bad cred"});
-        }
+        res.json({status: 'ok'});
 
     }
 
